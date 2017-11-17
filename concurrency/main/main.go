5e7513dd1fdae7de087cb1ae3f5a82877ba4be71
Maciek Niemczyk <Maciek.Niemczyk@vitraum.de>
2017-11-17 16:47:02 +0100

    init 0

--- concurrency/main/main.go
+++ concurrency/main/main.go
@@ -7,9 +7,12 @@ import (
 
 func main() {
 	goRoutines()
-	channels()
+	channels1()
+	channels2()
 	bufferedChannels()
 	rangeAndClose()
+	channelWithSelect()
+	defaultSelection()
 }
 
 /*
@@ -28,7 +31,7 @@ func main() {
 	Goroutines run in the same address space, so access to shared memory must be synchronized.
 	The sync package provides useful primitives.
 
- */
+*/
 
 func say(s string) {
 	for i := 0; i < 5; i++ {
@@ -74,17 +77,31 @@ func sum(s []int, c chan int) {
 	c <- sum // send sum to c
 }
 
-func channels() {
-	fmt.Println("\nchannels:")
+func channels1() {
+	fmt.Println("\nchannels1:")
 	s := []int{7, 2, 8, -9, 4, 0}
 
 	c := make(chan int)
-	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
-	x, y := <-c, <-c // receive from c
+	go sum(s[:len(s)/2], c) // c contains 17
+	go sum(s[len(s)/2:], c) // c contains 17 and -5
+	x, y := <-c, <-c        // receive from c
 
 	// TODO understand following:
 	// why does c puts its inside into y first and into x the second time?
+	// is it LIFO?
+
+	fmt.Println(x, y, x+y)
+}
+
+func channels2() {
+	fmt.Println("\nchannels2:")
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c) // c contains 17
+	x := <-c
+	go sum(s[len(s)/2:], c) // c contains -5
+	y := <-c
 
 	fmt.Println(x, y, x+y)
 }
@@ -100,7 +117,7 @@ func channels() {
 	Sends to a buffered channel block only when the buffer is full.
 	Receives block when the buffer is empty.
 
- */
+*/
 
 func bufferedChannels() {
 	fmt.Println("\nbufferedChannels:")
@@ -132,7 +149,7 @@ func bufferedChannels() {
 	Closing is only necessary when the receiver must be told there are no more values coming,
 	such as to terminate a range loop.
 
- */
+*/
 
 func fibonacci(n int, c chan int) {
 	x, y := 0, 1
@@ -147,9 +164,80 @@ func rangeAndClose() {
 	fmt.Println("\nrangeAndClose:")
 	c := make(chan int, 10)
 	go fibonacci(cap(c), c)
-	i := 1
+	i := 0
 	for f := range c {
 		fmt.Println(i, f)
 		i++
+	}
+}
+
+/*
+
+>>>> Select
+	The select statement lets a goroutine wait on multiple communication operations.
+
+	A select blocks until one of its cases can run,
+	then it executes that case. It chooses one at random if multiple are ready.
+
+*/
+
+func selectiveFibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+func channelWithSelect() {
+	fmt.Println("\nchannelWithSelect:")
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(i, <-c)
+		}
+		quit <- 0
+	}()
+	selectiveFibonacci(c, quit)
+}
+
+/*
+
+>>>> Default Selection
+	The default case in a select is run if no other case is ready.
+
+	Use a default case to try a send or receive without blocking:
+
+	select {
+	case i := <-c:
+		// use i
+	default:
+		// receiving from c would block
+	}
+
+*/
+
+func defaultSelection() {
+	fmt.Println("\ndefaultSelection:")
+	tick := time.Tick(100 * time.Millisecond)
+	// TODO is after only syntactical sugar here?
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
 	}
 }
