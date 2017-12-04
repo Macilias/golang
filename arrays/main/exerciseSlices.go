609cd09cc4016c3bd14f876e3df7fac985943778
Maciek Niemczyk <Maciek.Niemczyk@vitraum.de>
2017-12-04 15:01:52 +0100

    init 4: nice picture generator :)

--- arrays/main/exerciseSlices.go
+++ arrays/main/exerciseSlices.go
@@ -4,11 +4,11 @@ import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
 	res := make([][]uint8, dx)
-	for i := range res {
-		res[i] = make([]uint8, dy)
-		for j := range res[i] {
+	for x := range res {
+		res[x] = make([]uint8, dy)
+		for y := range res[x] {
 			// The choice of image is up to you. Interesting functions include (x+y)/2, x*y, and x^y.
-			res[i][j] = uint8((i ^ j) * 69)
+			res[x][y] = uint8((x ^ y) * 69)
 		}
 	}
 	return res
